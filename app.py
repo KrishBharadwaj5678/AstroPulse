0ed8aeb9607d80d89e58b9ce604ce78404ff7b67
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2024-07-19 13:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ if btn:
         if(time=="Monthly"):
             monthly_data=Call_API("monthly",None)
             horoscope_data(monthly_data)
-            Monthly_Data(f"Challenging Days: {monthly_data['data']['challenging_days']}", "#cf1515", "32px")
+            Monthly_Data(f"Challenging Days: {monthly_data['data']['challenging_days']}", "#F08080", "32px")
             Monthly_Data(f"Favorable Days: {monthly_data['data']['standout_days']}", "#32CD32", "33px")
             Monthly_Data(f"Month: {monthly_data['data']['month']}", "#00BFFF", "33px")
             
